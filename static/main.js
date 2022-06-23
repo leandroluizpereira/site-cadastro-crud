dda01d3b3d241498e2523afa6a95049e0c7a3f7b
leandroluizpereira <leandro.2306@hotmail.com>
2022-06-23 10:44:27 -0300

    index 2

--- static/main.js
+++ static/main.js
@@ -6,25 +6,30 @@ const openModal = () => document.getElementById('modal')
 const closeModal = () => document.getElementById('modal')
 .classList.remove('active')
 
+
+
 const tempClient = {
     nome: "Fulano",
     email: "fulano@gmail.com",
     celular: "99999999",
     cidade: "são roque"
-
-
 }
 
 // Crud - create update delete
 
-const createCliente = (clent) => {
+const createClient = (client) => {
 
-    localStorage.setItem("teste","teste para o crud")
-
+    localStorage.setItem("db_client",JSON.stringify(client))
 }
+
+
 
 //Eventos 
 document.getElementById('cadastrarCliente')
 .addEventListener('click',openModal)
 
 document.getElementById('click',closeModal)
+
+
+
+
